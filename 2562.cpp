44b2c1d2c92c5189031191afade1e0182cfa6cbb
Kuminchang <99315992+Kuminchang@users.noreply.github.com>
2022-03-02 15:33:07 +0900

    Add files via upload

---
+++ 2562.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)
+{
+	int num[9];
+	int i;
+	int a = 0;
+	int c;
+    int max=-1000000;
+	for (i = 0; i < 9; i++)
+	{
+		scanf_s("%d", &a);
+		num[i] = a;
+		
+        if (max < num[i])
+        {
+            max = num[i];
+			c = i;
+        }
+	}
+	printf("%d\n%d",max,c+1);
+	return 0;
+}
